8866b80d44c4b3bc46eecdf48aa1b2fbaca02748
nirapadak <naminopak@gmail.com>
2024-01-19 16:59:37 +0600

    Register editext add and error append status code

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -41,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     public RequestQueue requestQueue;
     ProgressBar progressBar;
+    Button button;
+    EditText nameEd, passwordEd, emailEd;
     String url = "https://sore-pear-fish-cuff.cyclic.app/api/v1/register";
 
 
@@ -53,15 +57,26 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.ResponseText);
         progressBar = findViewById(R.id.progress_circular);
+        button = findViewById(R.id.buttonClick);
+        nameEd = findViewById(R.id.nameEd);
+        passwordEd = findViewById(R.id.passwordEd);
+        emailEd = findViewById(R.id.emailEd);
 
 
         requestQueue = Volley.newRequestQueue(MainActivity.this);
 
+
+
+
+        // button click -----------------------------------
+
+        button.setOnClickListener(v->{
+            progressBar.setVisibility(View.VISIBLE);
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("name", "Volley");
-            jsonObject.put("password", "93485sl89");
-            jsonObject.put("email", "loveyoupal16@gmail.com");
+            jsonObject.put("name", nameEd.getText().toString());
+            jsonObject.put("password", passwordEd.getText().toString());
+            jsonObject.put("email", emailEd.getText().toString());
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
@@ -77,12 +92,23 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         // Handle the JSON response
                         progressBar.setVisibility(View.GONE);
+                        button.setVisibility(View.GONE);
                         try{
                             JSONObject jsonObject1 = new JSONObject(response.toString());
                             String token = jsonObject1.getString("token");
                             String user = jsonObject1.getString("user");
+                            // user Expend ----------name ---- email ------otp ----
+                            JSONObject jsonObject2 = new JSONObject(user);
+                            String name = jsonObject2.getString("name");
+                            String email = jsonObject2.getString("email");
+                            String otp = jsonObject2.getString("otp");
+
                             textView.append("\n"+token);
+                            textView.append("\n"+name);
+                            textView.append("\n"+email);
+                            textView.append("\n"+otp);
                             textView.append("\n\n"+user);
+
                         }catch (JSONException err){
                             textView.append("\n"+err.toString());
                         }
@@ -96,9 +122,13 @@ public class MainActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+
                         // Handle errors
                         progressBar.setVisibility(View.GONE);
-                        textView.setText(error.toString());
+                        textView.append("\n"+error.toString());
+                        textView.append("\n"+error.networkResponse.toString());
+                        textView.append("\n"+error.networkResponse.statusCode);
+                        textView.append("\n"+error.networkResponse.data);
                         Log.e(TAG, "JsonObjectRequest onErrorResponse: " + error.toString());
                     }
                 }){
@@ -110,7 +140,10 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        requestQueue.add(jsonObjectRequest);
+
+
+           requestQueue.add(jsonObjectRequest);
+       });
 
 
 
